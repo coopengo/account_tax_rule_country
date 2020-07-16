coopengo/account_payment

Fix flake8 errors and warnings
We add the flake8 configuration used so we ensure everyone uses the same.
We remove the usage of __all__ for non public API.
When possible, we rationalize the class name according to its __name__ and module.

issue9082
review297061002

setup.py
@@ -81,8 +81,8 @@ setup(name=name,
     keywords='tryton account payment',
     package_dir={'trytond.modules.account_payment': '.'},
     packages=(
-        ['trytond.modules.account_payment'] +
-        ['trytond.modules.account_payment.%s' % p for p in find_packages()]
+        ['trytond.modules.account_payment']
+        + ['trytond.modules.account_payment.%s' % p for p in find_packages()]
         ),
     package_data={
         'trytond.modules.account_payment': (info.get('xml', [])
@@ -96,7 +96,8 @@ setup(name=name,
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: Bulgarian',
         'Natural Language :: Catalan',
         'Natural Language :: Chinese (Simplified)',

tests/__init__.py
@@ -2,7 +2,7 @@
 # this repository contains the full copyright notices and license terms.
 
 try:
-    from trytond.modules.account_payment.tests.test_account_payment import suite
+    from trytond.modules.account_payment.tests.test_account_payment import suite  # noqa: E501
 except ImportError:
     from .test_account_payment import suite
 

coopengo/account_tax_rule_country

setup.py
@@ -82,8 +82,8 @@ setup(name=name,
     keywords='tryton account tax rule country',
     package_dir={'trytond.modules.account_tax_rule_country': '.'},
     packages=(
-        ['trytond.modules.account_tax_rule_country'] +
-        ['trytond.modules.account_tax_rule_country.%s' % p
+        ['trytond.modules.account_tax_rule_country']
+        + ['trytond.modules.account_tax_rule_country.%s' % p
             for p in find_packages()]
         ),
     package_data={
@@ -97,7 +97,8 @@ setup(name=name,
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: Bulgarian',
         'Natural Language :: Catalan',
         'Natural Language :: Chinese (Simplified)',

tests/__init__.py
@@ -2,7 +2,7 @@
 # this repository contains the full copyright notices and license terms.
 
 try:
-    from trytond.modules.account_tax_rule_country.tests.test_account_tax_rule_country import suite
+    from trytond.modules.account_tax_rule_country.tests.test_account_tax_rule_country import suite  # noqa: E501
 except ImportError:
     from .test_account_tax_rule_country import suite
 